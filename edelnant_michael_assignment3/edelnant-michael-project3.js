9df20b465582d370c4fdde73c1b55c41758040f1
mEdelnant <medelnant@fullsail.edu>
2012-11-07 22:23:25 -0500

    Everything is now migrated and creating dynamic objects from the JSON data.
    Next steps are to match it up to be more inline with the flowChart.

--- edelnant_michael_assignment3/edelnant-michael-project3.js
+++ edelnant_michael_assignment3/edelnant-michael-project3.js
@@ -11,34 +11,22 @@
 
 
 
-/* ###################################################
-Define Variables
-####################################################*/
-var myName = "Michael"; 								//string
-var opponentShortName = "Manuel"; 						//string
-var opponentFullName = "Manuel \"Speedy\" Lopez";		//escape string
-
-var activePlayer;										//string
-var matchLocation;										//string
-
-var DidIWinLag = false;									//boolean
-var matchComplete = false;								//boolean
-
-var solidSet = ["1","2","3","4","5","6","7","8"];		//array
-var stripeSet = ["9","10","11","12","13","14","15"];	//array
-
-
 
 /* ###################################################
-Build Match Object
+Build Primary Match Object
 ####################################################*/
 var matchObj = {
-	protagonist: "",
-	antagonist: "",
-	newRack: true,
-	isMatchStarted: false,
-	raceCount: 5,
-	rackCount: 0,
+	activePlayer	: "",										//string
+	protagonist		: "",										//string
+	antagonist		: "",										//string
+	matchLocation	: "",										//string
+	newRack 		: true,										//boolean
+	isMatchStarted 	: false,									//boolean	
+	matchComplete 	: false,									//boolean
+	raceCount 		: 5,										//number
+	rackCount 		: 0,										//number
+	solidSet 		: ["1","2","3","4","5","6","7","8"],		//array
+	stripeSet 		: ["9","10","11","12","13","14","15"], 		//array
 
 	/* ###################################################
 	Method: Procedure | Find Players
@@ -47,11 +35,24 @@ var matchObj = {
 		if (jSONObj) {
 			for (var key in jSONObj.players) {
 				var player = jSONObj.players[key];
-				console.log("First Name: " + player.fName + " Last Name: " + player.lName + " Nick Name: " + player.nName + " Skill Level " + player.SL);
-				console.log("Favorite Games");
-				for (var i=0, j=player.favGames.length; i<j; i++) {
-					console.log(player.favGames[i]);
-				}
+				if(parseInt(key) === 0) {
+					this.protagonist = newPlayer(player.fName);
+					this.protagonist.lName = player.lName;
+					this.protagonist.nName = player.nName;
+					this.protagonist.skillLevel = player.SL;
+					this.protagonist.favGames = player.favGames;
+					//console.log("First Name: " + this.protagonist.fName + " Last Name " + this.protagonist.fName +
+					//" NickName " + this.protagonist.nName + " FavGames: " + this.protagonist.favGames);
+				};
+				if(parseInt(key) === 1) {
+					this.antagonist = newPlayer(player.fName);
+					this.antagonist.lName = player.lName;
+					this.antagonist.nName = player.nName;
+					this.antagonist.skillLevel = player.SL;
+					this.antagonist.favGames = player.favGames;
+					//console.log("First Name: " + this.antagonist.fName + " Last Name " + this.antagonist.fName +
+					//" NickName " + this.antagonist.nName + " FavGames: " + this.antagonist.favGames);
+				}				
 			};        			
 	} else {
 			console.log("Looks like we will have to a make-up since there is not enough time to play our match.") };   
@@ -76,13 +77,13 @@ var matchObj = {
 	magicEightBall: function(playerName, opponentName) {
 		var RandomFlag = (Math.floor((Math.random()*2)+1));
 		if(RandomFlag === 1) {
-			activePlayer = playerName;
-			console.log(activePlayer + " won the lag! Lets get this match started!");
+			this.activePlayer = this.protagonist;
+			console.log(this.activePlayer.fName + " won the lag! Lets get this match started!");
 			console.log
 			console.log("--------------------------------------------------------");
 		} else {
-			activePlayer = opponentName;
-			console.log(activePlayer + " won the lag! Lets get this match started!");
+			this.activePlayer = this.antagonist;
+			console.log(this.activePlayer.fName + " won the lag! Lets get this match started!");
 			console.log("--------------------------------------------------------");
 		}
 
@@ -103,7 +104,7 @@ var matchObj = {
 			//Trip Flag
 			this.newRack = false;			
 			//Init breakRack
-			breakRack(activePlayer, (Math.floor((Math.random()*4)+1)));		
+			this.activePlayer.breakRack(this.activePlayer, (Math.floor((Math.random()*4)+1)));		
 
 		}
 	},
@@ -123,167 +124,154 @@ var matchObj = {
 	}	
 };
 
-
-
-
-
-
-
 /* ###################################################
-Not Good Enough Boolean Function:
-Check if Eight ball exists in array
+Define Person Constructor
 ####################################################*/
-var didMakeEightBall = function(argArray,argNumber) {
-    var i = argArray.length;
-    while (i--) {
-        //If 8 still exists in set, then set to false
-        if (parseInt(argArray[i]) == argNumber) { 
-        	return false;
-    	};
-    };
+var newPlayer = function(name) {
 
-    //Hey, looks like the 8 ball did fall!
-    console.log("Wait a sec! " + activePlayer + " made the 8 on the break! On to the next rack!");
-    console.log("--------------------------------------------------------");
-    //Trip Flag
-    newRack = true;
-    //Reset sets
-	solidSet = ["1","2","3","4","5","6","7","8"];		
-	stripeSet = ["9","10","11","12","13","14","15"];    
-    //Do it again!
-    matchObj.createNewRackProcess(newRack);
-    //else return true
-    return true;
-};
-
-/* ###################################################
-Good Enough Boolean Function:
-Check if Eight ball exists in array
-####################################################*/
-var didMakeBall = function(argNumber1,argNumber2) {
-    if(argNumber1 != argNumber2) {
-    	console.log("Wait a sec! " + activePlayer + " made the 8 on the break! On to the next rack!");
-	    console.log("--------------------------------------------------------");
-	    //If this so called ball happens to be the 8.. do...
-	    if(argNumber2 === 8){
-		    //Trip Flag
-		    newRack = true;
-		    //Reset sets
-			solidSet = ["1","2","3","4","5","6","7","8"];		
-			stripeSet = ["9","10","11","12","13","14","15"];    
-		    //Do it again!
-		    matchObj.createNewRackProcess(matchObj.newRack);
+	/* ###################################################
+	Method: Boolean
+	Check if Eight ball exists in array
+	####################################################*/
+	var didMakeBall = function(argNumber1,argNumber2) {
+	    if(argNumber1 != argNumber2) {
+	    	console.log("Wait a sec! " + matchObj.activePlayer.fName + " made the 8 on the break! On to the next rack!");
+		    console.log("--------------------------------------------------------");
+		    //If this so called ball happens to be the 8.. do...
+		    if(argNumber2 === 8){
+			    //Trip Flag
+			    matchObj.newRack = true;
+			    //Reset sets
+				matchObj.solidSet = ["1","2","3","4","5","6","7","8"];		
+				matchObj.stripeSet = ["9","10","11","12","13","14","15"];    
+			    //Do it again!
+			    matchObj.createNewRackProcess(matchObj.newRack);
+		    };
+		    //Yes, the ball you have inquired about has fallen
+		    return true;
+	    } else {
+	    	//No, the ball you have inquired about is still on the table
+	    	return false;
 	    };
-	    //Yes, the ball you have inquired about has fallen
-	    return true;
-    } else {
-    	//No, the ball you have inquired about is still on the table
-    	return false;
-    };
 
-};
-
-/* ###################################################
-Number Function:
-Wait function for match play. 
-####################################################*/
-var waitForMatch = function(argNumber) {
-	var i=0;
-	var matchLength = 45
-	var hours = 0;
-	var minutes = 0;
-	console.log("--------------------------------------------------------");
-	console.log("Prelude | Warm-up");
-	console.log("--------------------------------------------------------");
-
-	//iterate through matchCount
-	while(i++ < argNumber) {
-		//matchCountIteration multipled by matchLength gives us total minutes
-		minutes = i * matchLength;
-		//convert minutes to hours (floating point)
-		hours = minutes/60;
-		console.log(hours + " hours go by. Still Practicing.");
 	};
-	return hours;
-};
 
-/* ###################################################
-Array Function:
-Output random made balls and remove those specific 
-balls from the array being passed.
-####################################################*/
-var deleteBalls = function(argNumber, argArray) {
-	for (var i=0, j=argNumber; i < j; i++) {
-		//Random Index determined by initial set length of stripes or solids
-		var RandomIndex = (Math.floor((Math.random()*argArray.length)));
-		//Call out which balls were made
-		console.log(argArray[RandomIndex] + " ball falls...");
-		//Remove balls made
-		argArray.splice(RandomIndex, 1);
+	/* ###################################################
+	Method Function:
+	Wait function for match play. 
+	####################################################*/
+	var waitForMatch = function(argNumber) {
+		var i=0;
+		var matchLength = 45
+		var hours = 0;
+		var minutes = 0;
+		console.log("--------------------------------------------------------");
+		console.log("Prelude | Warm-up");
+		console.log("--------------------------------------------------------");
+
+		//iterate through matchCount
+		while(i++ < argNumber) {
+			//matchCountIteration multipled by matchLength gives us total minutes
+			minutes = i * matchLength;
+			//convert minutes to hours (floating point)
+			hours = minutes/60;
+			console.log(hours + " hours go by. Still Practicing.");
+		};
+		return hours;
+	};	
+
+	/* ###################################################
+	Array Function:
+	Output random made balls and remove those specific 
+	balls from the array being passed.
+	####################################################*/
+	var deleteBalls = function(argNumber, argArray) {
+		for (var i=0, j=argNumber; i < j; i++) {
+			//Random Index determined by initial set length of stripes or solids
+			var RandomIndex = (Math.floor((Math.random()*argArray.length)));
+			//Call out which balls were made
+			console.log(argArray[RandomIndex] + " ball falls...");
+			//Remove balls made
+			argArray.splice(RandomIndex, 1);
+		};
+		//Return manipulated array
+		return argArray;
+
 	};
-	//Return manipulated array
-	return argArray;
 
-};
-
-
-
-
-/* ###################################################
-Random Break Function:
-Random Scenario per break. Pass number of balls made and see what happens.
-####################################################*/
-var breakRack = function(playerName,ballCount) {
-	activePlayer = playerName;
-	ballCount;
-	if(ballCount) {
-		var RandomFlag = (Math.floor((Math.random()*3)+1));
-		console.log(playerName + " breaks!");
-		//More Random Goodiness
-		if(RandomFlag === 1) {
-			//Make Stripes
-			stripeSet = deleteBalls(ballCount, stripeSet);
-		} else if(RandomFlag === 2) {
-			//Else Make Solids
-			solidSet = deleteBalls(ballCount, solidSet);
-		} else {
-			//Super Break! Damn...
-			stripeSet = deleteBalls(Math.floor(ballCount), stripeSet);
-			solidSet =  deleteBalls(Math.floor(ballCount), solidSet);
-		}
-
-		//Handle Pluralisation
-		if(ballCount > 1) {
-			//Logical Operator
-			if(ballCount > 2 && RandomFlag === 3) {
-				console.log("SledgeHammer Break!")
-				console.log(playerName + " made " + (ballCount*2) + " balls on the break!");
-				console.log("I've never seen anything like it before!!")
+	/* ###################################################
+	Random Break Function:
+	Random Scenario per break. Pass number of balls made and see what happens.
+	####################################################*/
+	var breakRack = function(player,ballCount) {
+		matchObj.activePlayer = player;
+		var playerName = matchObj.activePlayer.fName;
+		ballCount;
+		if(ballCount) {
+			var RandomFlag = (Math.floor((Math.random()*3)+1));
+			console.log(playerName + " breaks!");
+			//More Random Goodiness
+			if(RandomFlag === 1) {
+				//Make Stripes
+				matchObj.stripeSet = this.deleteBalls(ballCount, matchObj.stripeSet);
+			} else if(RandomFlag === 2) {
+				//Else Make Solids
+				matchObj.solidSet = this.deleteBalls(ballCount, matchObj.solidSet);
 			} else {
-				console.log(playerName + " made " + ballCount + " balls on the break!");
+				//Super Break! Damn...
+				matchObj.stripeSet = this.deleteBalls(Math.floor(ballCount), matchObj.stripeSet);
+				matchObj.solidSet =  this.deleteBalls(Math.floor(ballCount), matchObj.solidSet);
+			}
+
+			//Handle Pluralisation
+			if(ballCount > 1) {
+				//Logical Operator
+				if(ballCount > 2 && RandomFlag === 3) {
+					console.log("SledgeHammer Break!")
+					console.log(playerName + " made " + (ballCount*2) + " balls on the break!");
+					console.log("I've never seen anything like it before!!")
+				} else {
+					console.log(playerName + " made " + ballCount + " balls on the break!");
+				};
+			} else {
+				console.log(playerName + " made " + ballCount + " ball on the break!");
 			};
+
+			//Check if 8 fell...
+			//Grab last element from the solidSet array since it will always be the 8.
+			this.didMakeBall(matchObj.solidSet[(matchObj.solidSet.length-1)],8);
+			return;
+			
 		} else {
-			console.log(playerName + " made " + ballCount + " ball on the break!");
+			//Womp Womp. Lame.
+			console.log(playerName + " made nothing on the break.")
+			if(matchObj.activePlayer === matchObj.protagonist.fName) {
+				matchObj.activePlayer = matchObj.antagonist.fName;
+			} else {
+				matchObj.activePlayer = matchObj.antagonist.fName;
+			};
 		};
 
-		//Check if 8 fell...
-		//Grab last element from the solidSet array since it will always be the 8.
-		didMakeBall(solidSet[(solidSet.length-1)],8);
-		return;
-		
-	} else {
-		//Womp Womp. Lame.
-		console.log(playerName + " made nothing on the break.")
-		if(activePlayer === myName) {
-			activePlayer = opponentShortName;
-		} else {
-			activePlayer = myName;
-		};
+	};		
+
+
+	return {
+		"fName" 		: name,					//string
+		"lName" 		: "",					//string
+		"nName" 		: "",					//string
+		"DidIWinLag" 	: false,				//boolean
+		"skillLevel"	: 0,					//number
+		"favGames"		: [],					//array
+		"didMakeBall"	: didMakeBall,			//Method
+		"waitForMatch"	: waitForMatch,			//Method 
+		"deleteBalls"	: deleteBalls,			//Method
+		"breakRack"		: breakRack				//Method
 	};
-
-
-	
 };
+
+
+matchObj.findPlayers(playerData);
+//matchObj.findGames(gameData);
 
 /* ###################################################
 Story Time:
@@ -293,9 +281,9 @@ Lets put everything together.
 //Set Setting
 	//Lets start out with the string function
 	
-	matchLocation = matchObj.stringFunction("Trickshots", "Winter Park, FL");
-	console.log("Hello, my name is " + myName + " and I would like to tell you a story.");
-	console.log("To set the scene, I was at " + matchLocation + " for my APA Billiards League.");
+	matchObj.matchLocation = matchObj.stringFunction("Trickshots", "Winter Park, FL");
+	console.log("Hello, my name is " + matchObj.protagonist.fName + " and I would like to tell you a story.");
+	console.log("To set the scene, I was at " + matchObj.matchLocation + " for my APA Billiards League.");
 	console.log("I had arrived there at around 7:45pm. 15min past our usual start time.");
 	console.log("I made my rounds and spoke with my captain. He mentioned that I would most likely play the 4th match.");
 
@@ -304,17 +292,17 @@ Lets put everything together.
 	
 	console.log("Since I had to wait 3 matches, I decided to grab a practice table.");
 	//Number Function coming up
-	console.log("Man "+ waitForMatch(3) + " hours is a long time to wait to play.. Grrr!");
+	console.log("Man "+ matchObj.protagonist.waitForMatch(3) + " hours is a long time to wait to play.. Grrr!");
 	console.log(" ");
 	
 
 //Yay, I get to play.
 	
-	console.log("I was finally up and my captain decided to match me against the other teams SL6.");
-	console.log("Since i'm an SL6 as well, I didn't mind the competetive play.");
-	console.log("and since we are both SL6's it was a " + matchObj.raceCount + "-" + matchObj.raceCount + " race.");
+	console.log("I was finally up and my captain decided to match me against the other teams SL" + matchObj.antagonist.skillLevel + ".");
+	console.log("Since i'm an SL" + matchObj.protagonist.skillLevel + " as well, I didn't mind the competetive play.");
+	console.log("and since we are both SL" + matchObj.protagonist.skillLevel + "'s it was a " + matchObj.raceCount + "-" + matchObj.raceCount + " race.");
 	console.log("First to " + matchObj.raceCount + " racks wins the match.");
-	console.log("My opponents name was " + opponentShortName + " otherwise known as " + opponentFullName);
+	console.log("My opponents name was " + matchObj.antagonist.fName + ".");
 	console.log("We both approached the table, shook hands and wished each other good luck.");
 	console.log("We then set up for our lag shots and got the match under way");
 	
@@ -335,7 +323,7 @@ It's Game Time!!
 		+ didMakeBall calls createNewRackProcess() if 8 ball is made. Rinse & Repeat. 
 	############################################*/
 	
-	matchObj.magicEightBall(myName,opponentShortName);
+	matchObj.magicEightBall(matchObj.protagonist.fName,matchObj.antagonist.fName);
 
 	console.log("...");
 	console.log("...stay tuned for more dramatic billiards play!...");
@@ -350,8 +338,7 @@ It's Game Time!!
 
 
 
-matchObj.findPlayers(playerData);
-matchObj.findGames(gameData);
+
 
 
 
